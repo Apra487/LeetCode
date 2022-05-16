c0f5c56726d86685f1cedeacd8c17ab832455722
Apratim <apra487@gmail.com>
2022-05-16 18:13:13 +0530

    Time: 86 ms (73.18%), Space: 43.1 MB (29.67%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,14 +3,19 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let obj = {};
-    for(let i = 0; i<nums.length; i++){
-        let comp = target - nums[i];
-        if(obj.hasOwnProperty(nums[i])){
-            return [obj[nums[i]],i];  
-        } 
-            obj[comp] = i;
-        // }
-    }
+
+var twoSum = function (nums, target) {
+	const compObj = {};
+
+	for (let index = 0; index < nums.length; index++) {
+
+        const element = nums[index]
+        const comp = target - element
+
+		if (compObj.hasOwnProperty(element)) {
+			return [compObj[element], index];
+		}
+		compObj[comp] = index;
+        
+	}
 };
